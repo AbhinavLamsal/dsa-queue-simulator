73af9308a76050972cfddf8e747cd300c67adb7b
Rumi-codes <rumisuwal@gmail.com>
2025-02-24 12:06:06 +0545

    update main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ void initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
     *window = SDL_CreateWindow("Traffic Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(*renderer, 144, 238, 144, 255); // Light blue
- 
+ //color changed
 }
 
 
